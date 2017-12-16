b83ff2c7dbdd385e9488dea23592e82b15dfa718
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-16 03:23:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ board = ["X"]
 def position_taken?(board,index)
   if board[index] == " " || ""
     return false
-  elsif board[index] != " " || ""
+  elsif board[index] == "X" || "O"
     return true
   end
 end
